9c02dd14a4fec9d34ecacb72f949ec2d5e12ff3f
reyhan <105841112623@gmail.com>
2025-08-28 22:37:44 +0800

    Commit kedua: update project

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,7 +1,4 @@
-// This file contains type definitions for your data.
-// It describes the shape of the data, and what data type each property should accept.
-// For simplicity of teaching, we're manually defining these types.
-// However, these types are generated automatically if you're using an ORM such as Prisma.
+
 export type User = {
   id: string;
   name: string;
@@ -17,13 +14,11 @@ export type Customer = {
 };
 
 export type Invoice = {
-  id: string;
+  id: string; 
   customer_id: string;
-  amount: number;
-  date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
+  amount: number; 
   status: 'pending' | 'paid';
+  date: string;
 };
 
 export type Revenue = {
